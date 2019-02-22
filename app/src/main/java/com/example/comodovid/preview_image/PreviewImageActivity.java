cf390df940e1c4dabdc30e68681162ebdd73282a
Sahil Garg <sahil.garg@daffodilsw.com>
2019-02-22 18:03:42 +0530

    added the basic packages and activities

---
+++ app/src/main/java/com/example/comodovid/preview_image/PreviewImageActivity.java
@@ -0,0 +1,15 @@
+package com.example.comodovid.preview_image;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.comodovid.R;
+
+public class PreviewImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preview_image);
+    }
+}
